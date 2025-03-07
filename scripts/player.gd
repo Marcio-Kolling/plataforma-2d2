1400d8b7326d96f736a2cd8c9ab760ee0e8681b2
Marcio-Kolling <marcio.kolling21@gmail.com>
2025-03-06 21:26:29 -0300

    AULA 06-03
    TILEMAP ADDED

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,7 +4,7 @@ extends CharacterBody2D
 
 
 const SPEED = 150.0
-const JUMP_VELOCITY = -400.0
+const JUMP_VELOCITY = -300.0
 
 
 func _physics_process(delta: float) -> void:
@@ -18,13 +18,13 @@ func _physics_process(delta: float) -> void:
 			anim.play("idle")
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		anim.play("jump")
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
